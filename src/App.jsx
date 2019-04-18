254d9ffb9eefca9c69f8c70978c59f261ea916d2
Alberto D.Sosa <albertodsosa@gmail.com>
2019-04-18 20:50:44 +0200

    Esqueleto inicial de la aplicación

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          
+        </header>
+        <section className="travels">
+          
+        </section>
+        <section className="contact">
+
+        </section>
+        <footer>
+
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
